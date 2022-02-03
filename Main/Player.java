6e11df2369fe2b15be2a0a302ac893212d521ad1
Jared Peters <69601052+JaredRPeters@users.noreply.github.com>
2022-02-03 13:35:17 -0700

    Update Player.java

--- Main/Player.java
+++ Main/Player.java
@@ -1,4 +1,4 @@
-package main;
+package blackJack;
 
 import java.awt.Font;
 import java.util.ArrayList;
@@ -17,14 +17,25 @@ public class Player extends JFrame{
 	int preferedWidth = 500;
 	int preferedHeight = 500;
 	
+	JLabel currentScoreLabel;
+	
 	Player(String name) {
 		this.name = name;
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle(name);
 		setSize(500, 500);
+		setLocation(1000, 500);
 		setResizable(false);
 		setVisible(true);
 		getContentPane().setLayout(null);
+		
+		JPanel currentVals = new JPanel();
+		currentScoreLabel = new JLabel("Current Score: " + String.valueOf(currentScore));
+		currentScoreLabel.setFont(new Font("Verdana",1,10));
+		currentVals.add(currentScoreLabel);
+		currentVals.setSize(100, 100);
+		currentVals.setLocation(200, 200);
+		add(currentVals);
 	}
 	
 	void giveCard(Card card) {
@@ -32,25 +43,36 @@ public class Player extends JFrame{
 		card.setLocation(1, 30 * hand.indexOf(card) + 1);
 		
 		add(card);
-		currentScore = 0;
-		for (Card c : hand) {
-			currentScore += c.val;
-		}
+		currentScore += card.val;
+		
+		currentScoreLabel.setText("Current Score: " + String.valueOf(currentScore));
+		
+		revalidate();
+		
 		if (currentScore > 21) {
 			busted = true;
-			System.out.println(name + " busted with " + currentScore);
 		} else if (currentScore == 21) {
-			System.out.println("Victory for " + name);
+			onScore();
 		}
-		
-		JPanel currentVals = new JPanel();
-		JLabel currentScoreLabel = new JLabel("Current Score: " + String.valueOf(currentScore));
-		currentScoreLabel.setFont(new Font("Verdana",1,10));
-		currentVals.add(currentScoreLabel);
-		currentVals.setSize(100, 100);
-		currentVals.setLocation(200, 200);
-		add(currentVals);
 
+	}
+	
+	void onScore() {
+		currentScoreLabel.setText("Current Score: " + String.valueOf(currentScore));
 		revalidate();
+		for (int i = 0; i < 10; i++) {
+			setTitle(name);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			setTitle("!!Hit 21!!");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
