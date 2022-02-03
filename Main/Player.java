5e32b11612131e9eacbb82ef9227d8df8119fc36
Jared Peters <69601052+JaredRPeters@users.noreply.github.com>
2022-02-02 22:44:03 -0700

    Update Player.java

--- Main/Player.java
+++ Main/Player.java
@@ -1,8 +1,11 @@
-package blackJack;
+package main;
 
+import java.awt.Font;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Player extends JFrame{
 	
@@ -26,10 +29,9 @@ public class Player extends JFrame{
 	
 	void giveCard(Card card) {
 		hand.add(card);
-		card.setSize(100, 100);
-		card.setLocation(1, 10 * hand.indexOf(card) + 1);
+		card.setLocation(1, 30 * hand.indexOf(card) + 1);
+		
 		add(card);
-		System.out.println(name + " gained " + card.val + card.suit);
 		currentScore = 0;
 		for (Card c : hand) {
 			currentScore += c.val;
@@ -40,5 +42,15 @@ public class Player extends JFrame{
 		} else if (currentScore == 21) {
 			System.out.println("Victory for " + name);
 		}
+		
+		JPanel currentVals = new JPanel();
+		JLabel currentScoreLabel = new JLabel("Current Score: " + String.valueOf(currentScore));
+		currentScoreLabel.setFont(new Font("Verdana",1,10));
+		currentVals.add(currentScoreLabel);
+		currentVals.setSize(100, 100);
+		currentVals.setLocation(200, 200);
+		add(currentVals);
+
+		revalidate();
 	}
 }
