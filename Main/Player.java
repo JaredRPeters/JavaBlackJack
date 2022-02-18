f6df14dfe7a361f0a1737a36c9767401918a0913
Jared Peters <69601052+JaredRPeters@users.noreply.github.com>
2022-02-18 11:19:45 -0700

    Update Player.java

--- Main/Player.java
+++ Main/Player.java
@@ -14,21 +14,44 @@ public class Player extends JFrame{
 	String name;
 	int currentScore = 0;
 	boolean busted = false;
+	boolean passed = false;
+	boolean hit21 = false;
 	
 	int preferedWidth = 500;
 	int preferedHeight = 500;
 	
 	JLabel currentScoreLabel;
 	
-	
+	Thread alterName = new Thread(() -> {
+		while(!busted) {
+			setTitle(name);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			setTitle("!!Hit 21!!");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		while(busted) {
+			if (!getName().equals("Busted")) {
+				setTitle("Busted");
+				break;
+			}
+		}
+	});
 	
 	Player(String name) {
 		this.name = name;
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle(name);
 		setSize(500, 500);
-		setLocation(1000, 500);
-		setResizable(false);
+		setLocation(0, 520);
+		setResizable(false); 
 		setVisible(true);
 		getContentPane().setLayout(null);
 		
@@ -41,44 +64,47 @@ public class Player extends JFrame{
 		add(currentVals);
 	}
 	
+	Player() {
+		//for var hold
+	}
+	
 	Boolean giveCard(Card card) {
-		if (!busted) {
-			Thread alterName = new Thread(() -> {
-				while(!busted) {
-					setTitle(name);
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					setTitle("!!Hit 21!!");
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			});
-			
-			hand.add(card);
-			card.setLocation(1, 30 * hand.indexOf(card) + 1);
-			
-			add(card);
-			currentScore += card.val;
-			
-			currentScoreLabel.setText("Current Score: " + String.valueOf(currentScore));
-			
-			revalidate();
-			
-			if (currentScore > 21) {
-				busted = true;
-				alterName.interrupt();
-				setTitle("Busted");
-			} else if (currentScore == 21) {
+		
+//		if (passed) {
+//			revalidate();
+//			return false;
+//		}
+//		
+//		if (!busted && !hit21) {
+//			revalidate();
+//			return false;
+//		}
+		
+		hand.add(card);
+		card.setLocation(card.cardWidth * hand.indexOf(card), this.getHeight() - (int)(card.cardHeight*(1.3f)));
+		
+		add(card);
+		card.repaint();
+		currentScore += card.val;
+		
+		currentScoreLabel.setText("Current Score: " + String.valueOf(currentScore));
+		
+		revalidate();
+		
+		if (currentScore > 21) {
+			busted = true;
+			setTitle("Busted");
+		} else if (currentScore == 21) {
+			hit21 = true;
+			if (!alterName.isAlive()) {
 				alterName.start();
+			} else {
+				alterName.interrupt();
 			}
-			return true;
+		} else {
+			setTitle(name);
 		}
-		return false;
+		revalidate();
+		return true;
 	}
 }
